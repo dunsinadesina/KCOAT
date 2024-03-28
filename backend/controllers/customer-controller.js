e9e9ac37edf3771b0d70e2495402161d36e51500
Jesudunsin23 <jesudunsinadesina@gmail.com>
2024-03-28 14:24:29 +0100

    update

--- backend/controllers/customer-controller.js
+++ backend/controllers/customer-controller.js
@@ -1,18 +1,9 @@
 const { Customer } = require('../model/customer');
 
 const insertCus = async (req, res) => {
+    const { cusName, username, email, phoneNumber, address, userpassword } = req.body;
     try {
-
-
-        const cusData = {
-            cusName: req.body.cusName,
-            username: req.body.username,
-            email: req.body.email,
-            phoneNumber: req.body.phoneNumber,
-            address: req.body.address,
-            password: req.body.userpassword,
-        };
-        if (!cusData.cusName || !cusData.username || !cusData.email || !cusData.phoneNumber || !cusData.address || cusData.password) {
+        if (!cusName || !username || !email || !phoneNumber || !address || userpassword) {
             return res.status(400).json({ message: "Please fill all the fields" });
         }
 
@@ -21,7 +12,7 @@ const insertCus = async (req, res) => {
             return res.status(400).json({ error: 'Customer with this email already exists' });
         }
 
-        //const hashedPassword = await bcrypt.hash(cusData.password, 10);
+        //const hashedPassword = await bcrypt.hash(password, 10);
 
         const newCustomer = await Customer.create(cusData);
         res.status(201).json({ message: "Customer created successfully", customer: newCustomer });
--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -3,22 +3,16 @@ const bcrypt = require('bcryptjs');
 const { getConnection, runQueryValues, loginSyntax } = require('../model/dbPool');
 const { CustomerAuth, Customer } = require('../model/customer');
 const {User} = require('../model/user')
-//Get the JWT secret from environment variable
 const secret = process.env.JWT_SECRET || "defaultSecret";
-
-//Define the login function
 const login = async (req, res) => {
-    //Extract username and password from the request body
     const { username, userpassword } = req.body;
     try {
-        //retrive customer info from the db
         const customer = await Customer.findOne({ where: { username } });
         if (!customer) {
             return res.status(404).json({ message: 'Username not found. Do you want to create an account?' });
         }
         //Compare the provided password with the stored hashed password
         const passwordMatch = await bcrypt.compare(userpassword, customer.password);
-        //If passwords match, generate a JWT token and return it
         if (passwordMatch) {
             const role = User.role;
             const token = jwt.sign({ username: customer.username, role }, secret);
@@ -32,5 +26,5 @@ const login = async (req, res) => {
         return res.status(500).json({ message: 'An error occurred during login' });
     }
 }
-//Export the login function
+
 module.exports = { login };
