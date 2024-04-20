683cdf9b7b6da4a1f04e48c661d577b8315666ef
Jesudunsin23 <jesudunsinadesina@gmail.com>
2024-04-20 13:42:44 +0100

    backend update

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -4,30 +4,24 @@ import { Admin } from '../model/admin.js';
 
 const secret = process.env.JWT_SECRET || "Tech4Dev";
 
-export const registerAdmin = async (req, res) => {
+export const adminLogin = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        // Check if admin with the same email already exists
-        const existingAdmin = await Admin.findOne({ where: { email: email } });
-        if (existingAdmin) {
-            return res.status(400).json({ message: 'Admin with this email already exists' });
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Fill all fields' });
         }
-
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create a new admin
-        const newAdmin = await Admin.create({
-            email: email,
-            password: hashedPassword,
-        });
-
-        // Generate a token
-        const token = jwt.sign({ id: newAdmin.id }, secret, { expiresIn: '1d' });
-
-        // Return the new admin along with the token
-        return res.status(201).json({ message: 'Admin registered successfully', admin: newAdmin, token: token });
+        const admin = await Admin.findOne({ where: { email } });
+        if (!admin) {
+            return res.status(404).json({ message: 'Admin not found' });
+        }
+        const passwordMatch = await bcrypt.compare(password, admin.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Wrong password' });
+        }
+        // Generate JWT token
+        const token = jwt.sign({ email: admin.email }, secret, { expiresIn: '1h' })
+        return res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
         console.error('Error registering admin:', error);
         return res.status(500).json({ message: 'An error occurred while registering admin' });
