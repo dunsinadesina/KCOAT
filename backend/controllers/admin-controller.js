33b2e09400ec26616c7db4575867c2e214d80fd6
Jesudunsin23 <jesudunsinadesina@gmail.com>
2024-04-20 13:57:59 +0100

    backend update

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -9,18 +9,22 @@ export const adminLogin = async (req, res) => {
 
     try {
         if (!email || !password) {
+            console.log('Fill all fields');
             return res.status(400).json({ message: 'Fill all fields' });
         }
         const admin = await Admin.findOne({ where: { email } });
         if (!admin) {
+            console.log('Admin not found');
             return res.status(404).json({ message: 'Admin not found' });
         }
         const passwordMatch = await bcrypt.compare(password, admin.password);
         if (!passwordMatch) {
+            console.log("Wrong password");
             return res.status(401).json({ message: 'Wrong password' });
         }
         // Generate JWT token
         const token = jwt.sign({ email: admin.email }, secret, { expiresIn: '1h' })
+        console.log('Login successful');
         return res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
         console.error('Error registering admin:', error);
