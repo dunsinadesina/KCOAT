2df398004615ae8e8600fad43986c0b5fd121bbb
Jesudunsin23 <jesudunsinadesina@gmail.com>
2024-04-24 12:53:34 +0100

    backend update

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -7,7 +7,6 @@ import cloudinaryV2 from './cloudinary.js';
 export const insertProduct = async (req, res) => {
     try {
         const { ProductName, ProductPrice, ProductDescription, ProductCategory, SubCategory, Quantity } = req.body;
-        const ProductImage = req.file;
         // Check if all required fields are provided
         if (!ProductName || !ProductPrice || !ProductDescription || !ProductCategory || !SubCategory || !Quantity) {
             return res.status(400).json({ message: "Fill in all fields" });
