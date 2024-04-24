f1e5c07263617056b86b220783038113e32ac87f
Jesudunsin23 <jesudunsinadesina@gmail.com>
2024-04-24 12:49:24 +0100

    backend update

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -13,7 +13,7 @@ export const insertProduct = async (req, res) => {
             return res.status(400).json({ message: "Fill in all fields" });
         }
         // Upload image to Cloudinary
-        const uploadResponse = await cloudinaryV2.uploader.upload(ProductImage.path, {
+        const uploadResponse = await cloudinaryV2.uploader.upload(ProductImage, {
             upload_preset: "kcoatstyle"
         })
         const imageUrl = uploadResponse.secure_url;
