6c6a32ea7959868b2bf7cf0589f1d0f9e3407a82
dunsinadesina <adesinajesudunsin@gmail.com>
2024-12-09 16:07:34 +0100

    backend update

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -5,17 +5,17 @@ import { Sequelize } from 'sequelize'; // Add the parentheses here
 //Initialize Sequelize with database credentials
 export const sequelize = new Sequelize(
     //'mysql://adminjs:adminjs@localhost:6000/adminjs',
-    "bnbzojiokmkdz5sxlsls", //database name
-    "uxhytwwevhvdtd8h", //username
-    "Ud4qzhPza6BdN3RQXs43", //password
+    "bz1pou7t8ldxtfxrpota", //database name
+    "uqdtlwshmdmzw692", //username
+    "1IinTQjo82iT6LCZU3sg", //password
     {
         dialect: "mysql",
-        host: "bnbzojiokmkdz5sxlsls-mysql.services.clever-cloud.com"
+        host: "bz1pou7t8ldxtfxrpota-mysql.services.clever-cloud.com"
     }
 );
 
-// sequelize.authenticate().then(() => {
-//     console.log("connected");
-// }).catch(err => {
-//     console.log(err);
-// });
+sequelize.authenticate().then(() => {
+    console.log("connected");
+}).catch(err => {
+    console.log(err);
+});
